35fa0c8fb974643b258d8798d603b37572dd8ccb
Dávid Csákvári <dodiehun@gmail.com>
2023-05-17 21:55:24 +0200

    Add readme and some coding functions

--- get_openai_completion.py
+++ get_openai_completion.py
@@ -6,7 +6,6 @@ import sys
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-
 # -------------------
 # Helper / formatting
 # -------------------
@@ -42,16 +41,27 @@ def get_unit_name(file_ext):
         return "Kotlin function"
     elif file_ext == "scala":
         return "Scala function"
+    elif file_ext == "sh":
+        return "Bash function"
     elif file_ext == "ts":
         return "TypeScript function"
     else:
         raise Exception("Unknown Language")
 
-def get_file_ext():
+def get_operation():
     if len(sys.argv) > 1:
         return sys.argv[1]
     else:
-        return "py"
+        return "complete"
+
+def get_file_ext():
+    return sys.argv[2]
+
+def execute_function(function_name):
+    function_name = function_name.strip()
+    function = globals().get(function_name)
+    if function:
+        return function()
 
 def remove_wrapping_triple_quotes(code):
     code_lines = code.split('\n')
@@ -83,6 +93,11 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
 # Text related
 # ------------
 
+def complete():
+    prompt = read_stdin()
+    return get_completion(prompt)
+
+
 def complete_sentence(text):
     (paragraph, hints) = split_paragraph_hint(text)
 
@@ -104,13 +119,22 @@ def complete_sentence(text):
         """
         return get_completion(prompt)
 
+def rephrase_text(text):
+    prompt = f"""
+    Rephrase this text to be clearer. The target audience is expected to be knowledgeable on the topic, and the purpose of this sentence is to share information:
+
+    ```
+    {text}
+    ```
+    """
+    return get_completion(prompt)
+
 # TODO: diff!
+# TODO: this does not work very reliable, and I could not make it much better with gpt 3.5. Maybe GPT4? Maybe different temp?
 def proofread(text):
     prompt = f"""
-    Proofread and correct the following text
-    and rewrite the corrected version. If you don't find
-    and errors, just say "No errors found". Don't use
-    any punctuation around the text:
+    Check the text delimited by triple backticks for grammatical and spelling mistakes.
+    Return a list of suggestions, or say "No errors found" in case you don't find anything to improve.
     ```{text}```
     """
     response = get_completion(prompt)
@@ -127,7 +151,9 @@ def proofread(text):
 # Coding related
 # --------------
 
-def write_function(spec, unit_name):
+def write_function():
+    spec = read_stdin()
+    unit_name = get_unit_name(get_file_ext())
     prompt = f"""
     Write a single {unit_name} that implements the specification \
     delimited by triple backticks.
@@ -138,7 +164,9 @@ def write_function(spec, unit_name):
     """
     return remove_wrapping_triple_quotes(get_completion(prompt))
 
-def implement_todos(spec, unit_name):
+def implement_todos_in_function():
+    spec = read_stdin()
+    unit_name = get_unit_name(get_file_ext())
     prompt = f"""
     Modify the following {unit_name} based on the instructions in TODO comments.
     If you find any functions in the code that you don't know about, just assume they are working correctly and leave them as they are.
@@ -153,7 +181,9 @@ def implement_todos(spec, unit_name):
 # TODO:
 # - Add iterative functions. (Usually being not a chatbot is good but for alternative and fix suggestion it might be good if it could remember.)
 #   - See chatbot chapter https://www.deeplearning.ai/short-courses/chatgpt-prompt-engineering-for-developers/
-def suggest_alternatives_for_function(code, unit_name):
+def suggest_alternatives_for_function():
+    spec = read_stdin()
+    unit_name = get_unit_name(get_file_ext())
     prompt = f"""
     Suggest 3 alternatives for the {unit_name} delimited by triple backticks. Write explanation and code.
     If you find any functions in the code that you don't know about, just assume they are working correctly and leave them as they are.
@@ -162,17 +192,21 @@ def suggest_alternatives_for_function(code, unit_name):
     """
     return get_completion(prompt)
 
-# TODO: see todo for suggest_alternatives_for_function
-def suggest_fixes_for_fixmes(code, unit_name):
+def review_function():
+    spec = read_stdin()
+    unit_name = get_unit_name(get_file_ext())
     prompt = f"""
-    Suggest fixes for the {unit_name} delimited by triple backticks.
-    Problems are added as FIXME comments.
+    Act as a technical reviewer and review the {unit_name} delimited by triple backticks.
+    Assume that the program works, but there are things that are not implemented optimally.
+    Suggest any improvements you find. If you don't find and improvement points, just say "No improvement points found".
 
     ```{spec}```
     """
     return get_completion(prompt)
 
-def write_unit_tests_for_function(code, unit_name):
+def write_unit_tests_for_function():
+    spec = read_stdin()
+    unit_name = get_unit_name(get_file_ext())
     prompt = f"""
     Write unit tests for the {unit_name} delimited by triple backticks.
 
@@ -180,26 +214,27 @@ def write_unit_tests_for_function(code, unit_name):
     """
     return get_completion(prompt)
 
-# TODO: remove. not very good on its own, use write_function with examples
-def write_function_for_unit_tests(code, unit_name):
-    prompt = f"""
-    Write the {unit_name} for the following test cases delimited by triple backticks.
+print(execute_function(get_operation()))
 
-    ```{spec}```
-    """
-    return get_completion(prompt)
-
-# TODO: remove. not very good on its own, use write_function with examples
-def write_function_for_examples(code, unit_name):
-    prompt = f"""
-    Write the {unit_name} that works like the following examples delimited by triple backticks.
-
-    ```{spec}```
-    """
-    return get_completion(prompt)
-
-spec = read_stdin()
-unit_name = get_unit_name(get_file_ext())
-print(write_function(spec, unit_name))
+# print(review_function(spec, unit_name))
 # print(proofread(spec))
 
+# print(complete_sentence(spec))
+# print(proofread(spec))
+# print(get_completion(spec))
+
+#TODO: simplify language for an informed reader tech document blog post
+# TODO: https://platform.openai.com/account/billing/overview
+# https://openai.com/pricing#language-models
+
+# TODO: idea: take whole post file as argument, mark a segment I'm working on that contains hints what to write there.
+
+
+
+# takes two numbers as parameters
+# - returns the sum of the parameters
+
+
+# Modify the following line to have "....", just return the code:
+
+# Todo Act as a technical reviewer...
--- readme.md
+++ readme.md
@@ -1,23 +1,50 @@
 # vim-openai
 
-wip
+## Install
+
+- Clone
+- Run `install.sh`
+- Add `get_openai_completion.sh` to your `$PATH`
+
+## Usage
+
+### CLI
+
+```bash
+prompt="..."
+echo "${prompt}" | get_openai_completion.sh <operation> <file_ext>
+```
+
+### Vim
+
+- Write prompt
+- Select text
+- `:terminal get_openai_completion.sh <operation> <file_ext>` to get results in a new terminal window will appear with the generated code
+- `:!get_openai_completion.sh <operation> <file_ext>` to replace selection with output
+
+## Parameters
+
+- `operation`:
+  - `complete` (default): sends the prompt without any modification
+  - Coding related:
+    - `write_function`
+    - `implement_todos_in_function`
+    - `review_function`
+    - `write_unit_tests_for_function`
+  - Writing related:
+    - `complete_sentence`
+    - `rephrase_text`
+    - `proofread`
+- `file_ext` should be the extension of the file you are working on
+
+## Examples
+
+### CLI
 
 ```
-# Usage
-# - Write spec as text
-# - Select text
-# - Run `:terminal ./run.sh` to get results in a new terminal window will appear with the generated code
-#   - :'<,'>terminal ./run.sh
-# - Run `:!./run.py` to replace selection with output
-#   - :'<,'>!./run.py
+echo "Create a dockerfile for nodejs and yarn" | get_openai_completion.sh complete
 ```
 
-```
-# Ideas:
-# - Use it as a Vim plugin
-#   - separate callable files/modes for the actions
-#   - vim plugin (commands / keys to call, auto-extract current filetype and pass it as an arg)
-#   - better name for the command
-#   - add to my dotfiles or a separate repo (maybe the vim plugin could pull it?)
-# - Add tests
-```
+### Vim
+
+TODO
